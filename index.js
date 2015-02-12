b174451877299b6b4dae7cbc11cc4d22b4b4af54
AaronKolenda <kolenda.a@gmail.com>
2015-02-12 11:28:15 -0500

    getting time and converting to hex

--- index.js
+++ index.js
@@ -26,3 +26,117 @@ var convertTimeframe = function(amount, unit) {
       return 0;
   }
 }
+
+
+var getHour = function(){
+  var d = new Date();
+  var currHour = d.getHours();
+  return currHour;
+}
+
+
+var getMin = function(){
+  var d = new Date();
+  var currMin = d.getMinutes();
+  return currMin;
+}
+
+
+var getSec = function(){
+  var d = new Date();
+  var currSec = d.getSeconds();
+  return currSec;
+}
+
+
+var getCurrentTime = function() {
+
+var hour = getHour();
+var min = getMin();
+var sec = getSec();
+
+if (hour < 10) {
+  hour = "0" + hour;
+}
+if (min < 10) {
+  min = "0" + min;
+}
+if (sec < 10) {
+  sec = "0" + sec;
+}
+
+var currTime = (hour + ":" + min + ":" + sec);
+
+//console.log(currTime);
+
+return currTime;
+
+}
+
+
+var getRGB = function() {
+
+  var rgb = (convertTimeframe(getHour(), "hour")  + " " + convertTimeframe(getMin(), "minute") + " " +
+  convertTimeframe(getSec(), "second"));
+
+  //console.log(rgb);
+  return rgb;
+
+}
+
+var getHex = function() {
+
+var RGBtemp = getRGB();
+var arrRGB = RGBtemp.split(" ");
+console.log(arrRGB);
+var r = parseInt(arrRGB[0]);
+var g = parseInt(arrRGB[1]);
+var b = parseInt(arrRGB[2]);
+
+var hex = rgbToHex(r, g, g);
+console.log(hex);
+return hex;
+}
+
+
+function componentToHex(c) {
+    var hex = c.toString(16);
+    return hex.length == 1 ? "0" + hex : hex;
+}
+
+function rgbToHex(r, g, b) {
+    return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
+
+
+
+
+setInterval(getCurrentTime, 1000);
+setInterval(getRGB, 1000);
+
+
+//$("#time").html(currTime);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
